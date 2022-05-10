2f0391387f5c4c7ca190f4deaab2e98a33b4bd7a
A A M Salehin Chowdhury <aamsalehin@gmail.com>
2022-05-10 11:55:53 +0600

    delete api added

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
       const result = await productCollection.insertOne(newItem);
       res.send(result);
     });
+    //delete one api
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
